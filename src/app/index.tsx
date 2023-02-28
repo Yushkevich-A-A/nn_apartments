8ee80b4a38c2e879ff8f4f30c67f003c357978f8
Anton <yushkevich.a.a@yandex.ru>
2023-02-28 21:45:50 +0300

    install react-router-dom

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import Header from 'widgets/header/ui';
 //import logo from './logo.svg';
 import './App.css';
-import { Switch, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Switch>
+      <Routes>
         
-      </Switch>
+      </Routes>
     </div>
   );
 }
