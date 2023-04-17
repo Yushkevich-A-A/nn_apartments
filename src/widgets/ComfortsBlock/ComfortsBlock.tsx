3d00ed27e0da240afa4965195545160b808d9c45
Anton <yushkevich.a.a@yandex.ru>
2023-04-17 23:51:57 +0300

    добавление элементов блока комфорт

--- src/widgets/ComfortsBlock/ComfortsBlock.tsx
+++ src/widgets/ComfortsBlock/ComfortsBlock.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import cn from 'classnames'
 import styles from './ComfortsBlock.module.scss';
+import security from './assert/key.svg';
+import park from './assert/park.svg';
+import towel from './assert/towel.svg';
+import tv from './assert/tv.svg';
+import furniture from './assert/wash.svg';
+import wifi from './assert/wifi.svg';
+import { css } from '@emotion/css';
 
 interface IProps {
     type: string,
@@ -7,11 +15,39 @@ interface IProps {
 };
 
 export const ComfortsBlock = ({comforts}: {comforts: IProps[]}) => {
+
+  const handleIcons = (type: string) => {
+    switch(type) {
+      case "wifi":
+        return wifi;
+      case "park":
+        return park;
+      case "tv":
+        return tv;
+      case "furniture":
+        return furniture;
+      case "linen":
+        return towel;
+      case "security":
+        return security;
+        default:
+          return null;
+    }
+  }
+
+
+
+
+
   return (
     <div className={styles["comforts-list"]}>
         {
-            comforts.map((item: IProps) => <div className="comfort-item">
-                {item.type} ____ {item.description}
+            comforts.map((item: IProps) => <div className={styles["comfort-item"]}>
+              <div className={styles["comfort-item_icon"]} style={{backgroundImage:`url(${handleIcons(item.type)})`}}>
+              </div>
+              <div className={styles["comfort-item_desc"]}>
+                {item.description}
+              </div>
             </div>)
         }
         
